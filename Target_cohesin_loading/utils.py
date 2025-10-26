a90058f0fbcaaa107f97de07aa65b0e9b36e5d3e
Hadi Rahmaninejad <rahmanin@discovery1.hpc.usc.edu>
2025-10-26 14:44:06 -0700

    adding some functions for calculation

---
+++ Target_cohesin_loading/utils.py
@@ -0,0 +1,61 @@
+import chromoscores.maputils as chrmap
+import chromoscores.snipping as chrsnip
+import chromoscores.scorefunctions as chrscores
+from scipy.stats import pearsonr
+
+def map_from_lefs(dset, sites_per_replica):
+    
+    ll = np.mod(dset.reshape((-1, 2)), sites_per_replica)
+    ll = ll[ll[:,1] > ll[:,0]]
+    
+    lmap = np.histogram2d(ll[:,0], ll[:,1], np.arange(sites_per_replica))[0]
+    
+    return (lmap + lmap.T)
+def peak_positions(boundary_lst_t, window_sizes=[1]):
+    peak_monomers = np.array([])
+    for i in window_sizes:
+        inds_to_add=[boundary_lst_t[j]+i for j in range(len(boundary_lst_t))]
+        peak_monomers = np.hstack((peak_monomers,inds_to_add))
+    return peak_monomers.astype(int)
+
+def FRiP(num_sites_t, lef_positions, peak_positions ):
+    
+    hist,edges = np.histogram(  lef_positions  , np.arange(num_sites_t+1) )
+    return np.sum(hist[peak_positions] )/len(lef_positions)
+def peak_positions(boundary_list, window_sizes=[1]):
+    """
+    Calculate peak positions based on a boundary_list within window_sizes.
+
+    Args:
+        boundary_list (list): List of boundary values.
+        window_sizes (list, optional): List of window sizes. Defaults to [1].
+
+    Returns:
+        np.ndarray: Array containing peak positions.
+    """
+    peak_monomers = np.array([])
+
+    for i in window_sizes:
+        inds_to_add = [boundary + i for boundary in boundary_list]
+        peak_monomers = np.hstack((peak_monomers, inds_to_add))
+
+    return peak_monomers.astype(int)
+def per_k_multiplier(multiplier, num_site=1, tot_site_per_k=4):
+    per_k_multiplier_conv = (1*multiplier*num_site+(tot_site_per_k-1))/tot_site_per_k
+    return per_k_multiplier_conv
+
+    
+def create_matrix(n, higher_value, lower_value):
+    # Create an n x n matrix filled with the lower value
+    matrix = np.full((n, n), lower_value)
+    
+    for i in range(n):
+        matrix[i, n - i - 1] = higher_value
+    
+    return matrix
+
+
+
+
+
+
